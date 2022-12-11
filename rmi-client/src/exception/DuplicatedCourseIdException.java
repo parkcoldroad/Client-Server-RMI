110c4d7adff6425c5b179337ea1d3cf4af782095
parkcoldroad <coldstreet@naver.com>
2022-12-11 11:40:29 +0900

    refactor: Delete unnecessary exceptions in client

--- rmi-client/src/exception/DuplicatedCourseIdException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class DuplicatedCourseIdException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public DuplicatedCourseIdException(String errorMessage) {
-		super(errorMessage);
-	}
-}
